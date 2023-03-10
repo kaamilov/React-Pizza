12564831511c5ac835b90b1b479009f4b5da7e0a
kaamilov <kaamilovmalik@gmail.com>
2023-03-11 01:04:28 +0300

    creating routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,22 +1,26 @@
-import Header from "../layout/Header";
 import { Container, styled } from "@mui/material";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import { Route, Routes } from "react-router-dom";
-import Cart from "../pages/Cart";
+import Cart from "../components/Cart";
 import { useState } from "react";
+import PizzaDetails from "../components/PizzaDetails";
+import MainLayout from "./MainLayout";
 const AppRoutes = () => {
   const [value, setValue] = useState("");
   return (
     <Styled_Container>
-      <Header value={value} setValue={setValue} />
-      <>
-        <Routes>
-          <Route path="/" element={<Home value={value} setValue={setValue}/>} />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route
+            path="/"
+            element={<Home value={value} setValue={setValue} />}
+          />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<PizzaDetails />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>
-      </>
+        </Route>
+      </Routes>
     </Styled_Container>
   );
 };
