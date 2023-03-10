23e2e84345dca52c0ee0546486adf9a767b937cf
kaamilov <kaamilovmalik@gmail.com>
2023-03-11 01:19:02 +0300

    changed to type script

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRoutes from "./routes/AppRoutes";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+};
+
+export default App;
